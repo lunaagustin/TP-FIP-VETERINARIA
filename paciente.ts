397f652cf723a29e8adf8609377c96ff5cc32421
juanamengochea <juamengo05@gmail.com>
2024-12-16 18:17:53 -0300

    agrego cambios

--- paciente.ts
+++ paciente.ts
@@ -1,35 +1,35 @@
-
 export class Paciente {
   protected nombre: string;
   protected especie: string;
   protected edad: number;
   protected esExotica: boolean = false;
-  protected idPaciente: number = 0;  
-
+  protected idPaciente: number = 0;
   constructor(nombre: string, especie: string, edad: number) {
     this.nombre = nombre;
-    this.especie = especie; 
+    this.especie = especie;
     this.setEspecie(especie);
     this.edad = edad;
   }
-
-
-  public setIdPaciente(id:number){
-    this.idPaciente = id; 
+  public getExotica(): boolean {
+    return this.esExotica;
   }
-
   public getIdPaciente():number{
-    return this.idPaciente; 
+    return this.idPaciente;
   }
-
   public getNombreMascota(): string {
     return this.nombre;
   }
-  public setNombreMascota(nombre: string) {
-    this.nombre = nombre;
-  }
   public getEspecie(): string {
     return this.especie;
+  }
+  public setIdPaciente(id:number){
+    this.idPaciente = id;
+  }
+  public getEdad(): number {
+    return this.edad;
+  }
+  public setNombreMascota(nombre: string) {
+    this.nombre = nombre;
   }
   public setEspecie(especie: string) {
     this.especie = especie;
@@ -38,9 +38,6 @@ export class Paciente {
     } else {
       this.esExotica = false;
     }
-  }
-  public getEdad(): number {
-    return this.edad;
   }
   public setEdad(edad: number) {
     this.edad = edad;
